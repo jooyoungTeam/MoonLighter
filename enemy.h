38b1d4a6af5f196fdc491a1b8ca14bc4451c4671
해라 강 <gofk852@naver.com>
2020-08-06 18:36:21 +0900

    에너미 이미지 추가

--- enemy.h
+++ enemy.h
@@ -3,5 +3,20 @@
 class enemy :
 	public gameNode
 {
+private:
+	FloatRect _rc;
+	
+	float _x, _y;
+	Image* _img;
+	animation* _motion;
+
+public:
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+
+
 };
 
