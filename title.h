9f2f4ff29577e774858a20ef2eb584bd8c971383
ycfzi <ycfzi@users.noreply.github.com>
2020-08-07 16:15:01 +0900

    UI 이미지 추가, title scene 수정, START->Shop Scene, MAPTOOL->Maptool Scene, EXIT->Dungeon Scene

--- title.h
+++ title.h
@@ -1,8 +1,36 @@
 #pragma once
 #include "gameNode.h"
+
+#define MENUNUM 3
+
+enum class SELECT
+{
+	MOVE, STOP, STAY
+};
+
+struct tagMenu
+{
+	FloatRect rc;
+	wstring name;
+};
+
 class title :public gameNode
 {
 private:
+	Image* _titleFront;
+	Image* _titleBack;
+	Image* _menuBox;
+	Image* _logo;
+
+	tagMenu _menu[MENUNUM];
+	SELECT _menuSelect;
+	Image* _selectImg;
+	int _select;
+	int _selectX;
+	int _count;
+
+	int _frameCount;
+	int _frameX;
 
 public:
 	title() {}
@@ -12,5 +40,7 @@ public:
 	void update();
 	void render();
 	void release();
+
+	void draw();
 };
 
