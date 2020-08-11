0502242487bf17f0ddb804f184fc8569fde0d725
까말 <wsok147@naver.com>
2020-08-11 19:59:18 +0900

    플레이어 상태수정

--- player.cpp
+++ player.cpp
@@ -14,6 +14,7 @@ HRESULT player::init()
 	_swim = new playerSwimState();
 	_bow = new playerbowState();
 	_sword = new playerSwordState();
+	_broom = new playerBroomState();
 	_arrow = new arrow;
 	_arrow->init();
 	_index = 0;
@@ -219,6 +220,14 @@ void player::animationLoad()
 	_playerImg = ImageManager::GetInstance()->AddFrameImage("playerUpSwim", L"image/player/swimState.png", 10, 4);
 	int playerUpSwim[] = { 31, 32, 33, 34, 35, 36, 37, 38, 39 };
 	KEYANIMANAGER->addArrayFrameAnimation(_index, "playerUpSwim", "playerUpSwim", playerUpSwim, 9, 13, true);
+
+	//빗자루 개싹수;
+	_playerImg = ImageManager::GetInstance()->AddFrameImage("playerBroom", L"image/player/broomState.png", 119, 1);
+	int playerBroom[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+	31, 32, 33, 34, 35, 36, 37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,
+	74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,
+	114,115,116,117,118};
+	KEYANIMANAGER->addArrayFrameAnimation(_index, "playerBroom", "playerBroom", playerBroom, 119, 13, true);
 }
 
 //활나가는 방향
